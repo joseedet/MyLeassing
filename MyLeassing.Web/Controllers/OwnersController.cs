bbbfbaf8204cc5d63f61e64eba49182ef77b8ebc
Jose Edet <joseedet@gmail.com>
2019-10-10 18:53:15 +0100

    Delete property

--- MyLeassing.Web/Controllers/OwnersController.cs
+++ MyLeassing.Web/Controllers/OwnersController.cs
@@ -503,6 +503,32 @@ namespace MyLeassing.Web.Controllers
             return RedirectToAction($"{nameof(DetailsProperty)}/{contract.Property.Id}");
         }
 
+        public async Task<IActionResult> DeleteProperty(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var property = await _dataContext.Properties
+                .Include(p => p.Owner)
+                .Include(p => p.PropertyImages)
+                .Include(p => p.Contracts)
+                .FirstOrDefaultAsync(pi => pi.Id == id.Value);
+            if (property == null)
+            {
+                return NotFound();
+            }
+            if(property.Contracts.Count !=0)
+            {
+                ModelState.AddModelError(string.Empty, "The property can't delete!");
+            }
+            _dataContext.PropertyImages.RemoveRange(property.PropertyImages);
+            // _dataContext.Contracts.RemoveRange(property.Contracts);
+            _dataContext.Properties.Remove(property);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction($"{nameof(Details)}/{property.Owner.Id}");
+        }
 
 
 
