371a6e97ae87f8ea48e163f8737c75248eb5ce56
Jose Edet <joseedet@gmail.com>
2019-10-13 15:22:39 +0100

    Property Image URL

--- MyLeassing.Web/Data/Entities/PropertyImage.cs
+++ MyLeassing.Web/Data/Entities/PropertyImage.cs
@@ -17,6 +17,6 @@ namespace MyLeassing.Web.Data.Entities
         public Property Property { get; set; }
 
         // TODO: Change the path when publish
-        public string ImageFullPath => string.IsNullOrEmpty(ImageUrl) ?null : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+        public string ImageFullPath => string.IsNullOrEmpty(ImageUrl) ?null : $"https://myleassing.azurewebsites.net{ImageUrl.Substring(1)}";
     }
 }
